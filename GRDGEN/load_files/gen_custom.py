0bf9279727f6f9009c63a0f19f64688f13117d6d
Carolina Hurtado-Pulido <carolinah0308@hotmail.com>
2026-01-15 22:15:14 -0500

    change extent of load grid

--- GRDGEN/load_files/gen_custom.py
+++ GRDGEN/load_files/gen_custom.py
@@ -50,6 +50,12 @@ squares = squares.drop(columns=["type"])
 squares["id"] = squares["id"].astype(int)
 squares.set_index("id", inplace=True)
 squares.sort_index(inplace=True)
+
+# CH: The original script creates a grid between -90 to 91 in lat and 0 to 360 in lon. Creating that grid takes a lot of time and I don't need that. Then use the variables to change the extent of the load grid
+ext_n= 31.
+ext_s = 30.
+ext_e = 269.5
+ext_w = 268.
 # Heighs MR
 # heights =[8.41, 8.47, 10.71, 10.87, 10.56, 9.4, 6.93, 5.05, 4.31, 4.28, 4.93, 5.26]
 # Heighs AR  [4.63, 4.84, 4.23, 4.78, 4.44, 3.79, 3.89, 4.01, 3.56, 3.44, 3.36, 3.98], just use the month with more and less water (feb and nov)
@@ -108,7 +114,7 @@ for h in heights:
         write_nc = True
 
         # Write Load Information to a Text File? (Alternative for convolution)
-        write_txt = False
+        write_txt = True
 
         # Write Load Information to a GMT-formatted File? (Lon, Lat, Amplitude)
         write_gmt = False
@@ -243,10 +249,11 @@ for h in heights:
 #  :: In general, the longitude range should be [0,360]
 #  :: In the special case that the bounding box crosses the prime meridian,
 #     the range should be [-180,0] for wlon and [0,180] for elon
-wlon= 360.+(-91.2277957) # range [0,360] | If Bounding Box Crosses Prime Meridian, range = [-180,0]
-elon= 360.+(-91.2172368) # range [0,360] | If Bounding Box Crosses Prime Meridian, range = [0,180]
-slat= 30.3089489  # range [-90,90]
-nlat= 30.31811  # range [-90,90]
+wlon= -90.9770750 #360.+(-91.2277957) # range [0,360] | If Bounding Box Crosses Prime Meridian, range = [-180,0]
+elon= -90.9779771 # 360.+(-91.2172368) # range [0,360] | If Bounding Box Crosses Prime Meridian, range = [0,180]
+slat= 30.4378743 #30.3089489  # range [-90,90]
+nlat= 30.438776 #30.31811  # range [-90,90]
+
 
 # Apply Prime-Meridian Correction?
 #  :: Set to "True" if the Bounding Box Stradles the Prime Meridian
@@ -254,7 +261,7 @@ nlat= 30.31811  # range [-90,90]
 pm_correct = False
 
 # Specify Load Height (meters)
-loadamp=8.41
+loadamp=4.84
 
 # Specify Phase (deg)
 loadpha=0.0
@@ -275,10 +282,10 @@ gspace = 0.0005
 
 # Output Filename
 
-outfile = ("AR_load_BRregion_TEST"+str(loadamp)+"_area37")
+outfile = ("AR_load_BRregion_TEST"+str(loadamp)+"_area137")
 
 # Write Load Information to a netCDF-formatted File? (Default for convolution)
-write_nc = False
+write_nc = True
 
 # Write Load Information to a Text File? (Alternative for convolution)
 write_txt = True
@@ -318,8 +325,8 @@ if initial_grid is not None:
     llat,llon,amp,pha,lat1dseq,lon1dseq,amp2darr,pha2darr = read_AmpPha.main(initial_grid,regular_grid=regular_grid)
 # Generate New Grid
 else:
-    lats = np.arange(30.,31.,gspace) + (gspace/2.0)
-    lons = np.arange(268.,269.,gspace) + (gspace/2.0)
+    lats = np.arange(ext_s, ext_n, gspace) + (gspace/2.0)
+    lons = np.arange(ext_w, ext_e, gspace) + (gspace/2.0)
     xv,yv = np.meshgrid(lons,lats)
     llon = np.ravel(xv)
     llat = np.ravel(yv)
